5b90e0a1a5cd3e35ee9e920b205143318c54c213
Devrath <166014800+devrath-teg@users.noreply.github.com>
2025-05-08 20:12:13 +0530

    added controls

---
+++ code/app/src/main/java/com/economist/demo/pip/screens/activities/PiPActionReceiver.kt
@@ -0,0 +1,24 @@
+package com.economist.demo.pip.screens.activities
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+/*
+class PiPActionReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            VideoPlayerFullScreenActivity.ACTION_PLAY -> {
+                // Trigger play logic (you may delegate to a ViewModel or service)
+                Toast.makeText(context, "Play clicked", Toast.LENGTH_SHORT).show()
+            }
+
+            VideoPlayerFullScreenActivity.ACTION_PAUSE -> {
+                // Trigger pause logic
+                Toast.makeText(context, "Pause clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+*/
--- code/app/src/main/java/com/economist/demo/pip/screens/activities/VideoPlayerFullScreenActivity.kt
+++ code/app/src/main/java/com/economist/demo/pip/screens/activities/VideoPlayerFullScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.economist.demo.pip.screens.activities
 
+import android.app.PendingIntent
 import android.app.PictureInPictureParams
+import android.app.RemoteAction
+import android.content.Intent
+import android.content.Intent.*
+import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.graphics.Rect
+import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Rational
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,6 +25,12 @@ import com.economist.demo.pip.screens.fragments.VideoPlayerFragment
 
 @androidx.annotation.OptIn(UnstableApi::class)
 class VideoPlayerFullScreenActivity : AppCompatActivity() {
+
+    private val isPipSupported by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            packageManager.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE)
+        } else false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,18 +68,70 @@ class VideoPlayerFullScreenActivity : AppCompatActivity() {
 
 
     fun enterPipMode() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val aspectRatio = Rational(16, 9)
-            val pipParams = PictureInPictureParams.Builder()
-                .setAspectRatio(aspectRatio)
-                .build()
-            enterPictureInPictureMode(pipParams)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            enterPictureInPictureMode()
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val params = getPipParams()
+            enterPictureInPictureMode(params)
         }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun getPipParams(isPlaying: Boolean = false): PictureInPictureParams {
+        val rect = Rect()
+        //binding.player.getGlobalVisibleRect(rect)
+
+        val builder = PictureInPictureParams.Builder()
+            .setAspectRatio(Rational(16, 9))
+            .setSourceRectHint(rect)
+            .setActions(
+                listOfNotNull(
+                    getPipAction()
+                )
+            )
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            builder.setAutoEnterEnabled(isPlaying)
+        }
+
+        return builder.build()
+    }
+
+    private fun getPipAction(): RemoteAction? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val actionUri = Uri.parse("https://youtube.com/AndroidDeveloperTips")
+            val actionIntent = Intent(ACTION_VIEW, actionUri)
+            val actionPendingIntent =
+                PendingIntent.getActivity(this, 0, actionIntent, PendingIntent.FLAG_IMMUTABLE)
+            val remoteAction = RemoteAction(
+                Icon.createWithResource(this, R.drawable.ic_launcher_foreground),
+                "More info",
+                "More info action",
+                actionPendingIntent
+            )
+            remoteAction
+        } else null
     }
 
     override fun onUserLeaveHint() {
         super.onUserLeaveHint()
-        enterPipMode()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            // the PiP auto enter feature is not available and we should do it manually
+            enterPipMode()
+        }
+    }
+
+
+    override fun onPictureInPictureModeChanged(isInPictureInPictureMode: Boolean, newConfig: Configuration) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            super.onPictureInPictureModeChanged(isInPictureInPictureMode, newConfig)
+        }
+        if (isInPictureInPictureMode) {
+           // Hide actions
+
+        } else {
+            // Show actions
+        }
     }
 }
 
